e0584154d785168105f4d411ba8f2ffdfa8c6ac4
Jose Bezerra <jbqneto@gmail.com>
2022-04-24 13:21:59 +0100

    Creating some tests for the form

--- src/main/java/io/jbqneto/favorites/application/domain/model/business/Category.java
+++ src/main/java/io/jbqneto/favorites/application/domain/model/business/Category.java
@@ -1,6 +1,7 @@
 package io.jbqneto.favorites.application.domain.model.business;
 
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 public class Category implements IDModel {
     private final long id;
@@ -31,6 +32,19 @@ public class Category implements IDModel {
     public Category(long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     public int getCountFavorites() {
